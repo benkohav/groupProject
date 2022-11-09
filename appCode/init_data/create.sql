91082c0925918847026b3f53075ff6cad05e9b96
Ryan Hannigan <ryha4584@colorado.edu>
2022-11-09 15:18:23 -0700

    testing db

--- appCode/init_data/create.sql
+++ appCode/init_data/create.sql
@@ -1,73 +1,112 @@
-CREATE TABLE IF NOT EXISTS User (
-  userID INT NOT NULL,
-  username VARCHAR(45) NULL,
-  password CHAR(60) NULL,
-  email VARCHAR(45) NULL,
-  phone BIGINT(10) NULL,
-  PRIMARY KEY (userID));
 
-CREATE TABLE IF NOT EXISTS Item (
-  ItemId INT NOT NULL,
-  ItemName VARCHAR(45) NULL,
-  ItemDescription VARCHAR(200) NULL,
-  Condition VARCHAR(200) NULL,
-  CategoryID INT NULL,
-  PRIMARY KEY (ItemId),
-  CONSTRAINT Cat
-    FOREIGN KEY (CategoryID)
-    REFERENCES Category (CategoryID)
+-- -----------------------------------------------------
+-- Table 'mydb'.'customerDimension'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'mydb'.'customerDimension' ;
+
+CREATE TABLE IF NOT EXISTS 'mydb'.'customerDimension' (
+  'customer_ID' INT NOT NULL,
+  'username' VARCHAR(45) NOT NULL,
+  'email' VARCHAR(45) NULL,
+  PRIMARY KEY ('customer_ID'))
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'dateDimension'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'mydb'.'dateDimension' ;
+
+CREATE TABLE IF NOT EXISTS 'mydb'.'dateDimension' (
+  'time_ID' INT NOT NULL,
+  'date' DATE NULL,
+  'day' INT NULL,
+  'month' INT NULL,
+  'year' INT NULL,
+  PRIMARY KEY ('time_ID'))
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'imageOfCategory'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'mydb'.'imageOfCategory' ;
+
+CREATE TABLE IF NOT EXISTS 'mydb'.'imageOfCategory' (
+  'type_ID' INT NOT NULL,
+  'imageURL' VARCHAR(45) NULL,
+  PRIMARY KEY ('type_ID'))
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'categoryDimension'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'mydb'.'categoryDimension' ;
+
+CREATE TABLE IF NOT EXISTS 'mydb'.'categoryDimension' (
+  'category_ID' INT NOT NULL,
+  'type_ID' INT NOT NULL,
+  PRIMARY KEY ('category_ID'),
+  INDEX 'fk_categoryDimension_imageOfCategory1_idx' ('type_ID' ASC) VISIBLE,
+  CONSTRAINT 'fk_categoryDimension_imageOfCategory1'
+    FOREIGN KEY ('type_ID')
+    REFERENCES 'mydb'.'imageOfCategory' ('type_ID')
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
 
-CREATE TABLE IF NOT EXISTS Image (
-  ImageID INT NOT NULL,
-  URL VARCHAR(45) NULL,
-  CategoryID INT NULL,
-  PRIMARY KEY (ImageID),
-  CONSTRAINT Category
-    FOREIGN KEY (CategoryID)
-    REFERENCES Category (CategoryID)
-    ON DELETE SET NULL
-    ON UPDATE NO ACTION);
 
-CREATE TABLE IF NOT EXISTS ItemHistory (
-  HistoricalOrderID INT NOT NULL,
-  ItemID INT NOT NULL,
-  userID INT NOT NULL,
-  timeBorrowed DATETIME NULL,
-  timeReturned DATETIME NULL,
-  INDEX Item_idx (ItemID ASC) VISIBLE,
-  PRIMARY KEY (HistoricalOrderID),
-  CONSTRAINT userID
-    FOREIGN KEY (userID)
-    REFERENCES User (userID)
+-- -----------------------------------------------------
+-- Table 'mydb'.'productDimension'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'mydb'.'productDimension' ;
+
+CREATE TABLE IF NOT EXISTS 'mydb'.'productDimension' (
+  'product_ID' INT NOT NULL,
+  'quantity' INT NULL,
+  'unit_price' DECIMAL(10,2) NULL,
+  'description' VARCHAR(90) NULL,
+  PRIMARY KEY ('product_ID'))
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'Fact Table'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'mydb'.'Fact Table' ;
+
+CREATE TABLE IF NOT EXISTS 'mydb'.'Fact Table' (
+  'Transaction_ID' INT NOT NULL,
+  'customer_ID' INT NOT NULL,
+  'product_ID' INT NOT NULL,
+  'time_ID' INT NOT NULL,
+  'product_ID' INT NOT NULL,
+  'order_ID' INT NOT NULL,
+  'totalPrice' DECIMAL(10,2) NULL,
+  'timeRented' INT NULL,
+  PRIMARY KEY ('Transaction_ID'),
+  INDEX 'fk_Fact Table_customerDimension_idx' ('customer_ID' ASC) VISIBLE,
+  INDEX 'fk_Fact Table_dateDimension1_idx' ('product_ID' ASC) VISIBLE,
+  INDEX 'fk_Fact Table_categoryDimension1_idx' ('time_ID' ASC) VISIBLE,
+  INDEX 'fk_Fact Table_productDimension1_idx' ('product_ID' ASC) VISIBLE,
+  CONSTRAINT 'fk_Fact Table_customerDimension'
+    FOREIGN KEY ('customer_ID')
+    REFERENCES 'mydb'.'customerDimension' ('customer_ID')
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT Item
-    FOREIGN KEY (ItemID)
-    REFERENCES Item (ItemId)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-CREATE TABLE IF NOT EXISTS Fee (
-  userID INT NOT NULL,
-  HistoricalOrderID INT NOT NULL,
-  amount DECIMAL(7,2) NULL,
-  paid TINYINT NULL DEFAULT 0,
-  payby DATE NULL,
-  PRIMARY KEY (userID, HistoricalOrderID),
-  INDEX ItemHistory_idx (HistoricalOrderID ASC) VISIBLE,
-  CONSTRAINT User
-    FOREIGN KEY (userID)
-    REFERENCES User (userID)
+  CONSTRAINT 'fk_Fact Table_dateDimension1'
+    FOREIGN KEY ('product_ID')
+    REFERENCES 'mydb'.'dateDimension' ('time_ID')
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT ItemHistory
-    FOREIGN KEY (HistoricalOrderID)
-    REFERENCES ItemHistory (HistoricalOrderID)
+  CONSTRAINT 'fk_Fact Table_categoryDimension1'
+    FOREIGN KEY ('time_ID')
+    REFERENCES 'mydb'.'categoryDimension' ('category_ID')
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION,
+  CONSTRAINT 'fk_Fact Table_productDimension1'
+    FOREIGN KEY ('product_ID')
+    REFERENCES 'mydb'.'productDimension' ('product_ID')
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 
 
-
-
+-- SET SQL_MODE=@OLD_SQL_MODE;
+-- SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+-- SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
