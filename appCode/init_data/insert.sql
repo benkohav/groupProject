e1e854c08d09daf5024cb51f0333e4eec8c2ab8c
sesh9096 <sesh9096@colorado.edu>
2022-11-09 12:30:28 -0700

    updates to index

---
+++ appCode/init_data/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO Category (CategoryName, CategoryDescription, Brand, SubcategoryID) VALUES
+('iPad', 'A tablet manufactured by Apple', 'Apple', 2),
+('Tablet', 'A flat screen', NULL, NULL);
