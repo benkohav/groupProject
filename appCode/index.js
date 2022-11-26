850db7b888cf199db9013aaac9187069da88df98
sesh9096 <sesh9096@colorado.edu>
2022-11-26 14:52:08 -0700

    search filtering working

--- appCode/index.js
+++ appCode/index.js
@@ -208,81 +208,48 @@ const dbConfig = {
   
     //Rendering search
     app.get('/search', (req, res) => {
-      const search = "";
-      var query = `SELECT Item.userID as userid, Item.ItemID, SubCategory.CategoryName as subcatname, SuperCategory.CategoryName as catname, SuperCategory.CategoryDescription, SubCategory.Brand, URL, usercart.userID AS incart
-      FROM Item 
-      INNER JOIN Category SubCategory ON Item.CategoryID = SubCategory.CategoryID 
-      LEFT OUTER JOIN Category SuperCategory ON SubCategory.SuperCategoryID = SuperCategory.CategoryID 
-      LEFT OUTER JOIN Image ON SubCategory.CategoryID = Image.CategoryID
-      LEFT OUTER JOIN (SELECT * FROM Cart WHERE UserID = $2) AS usercart ON usercart.ItemID = Item.ItemID
-        WHERE SubCategory.Brand LIKE $1
-        OR SubCategory.CategoryName LIKE $1
-        OR SuperCategory.CategoryName LIKE $1;`
       if(!req.session.user)
       {
         res.render('pages/login',{message: 'Error. No user logged in currently.'} );
       }
-      if(req.query.filter == "available")
-      {
-        console.log("entered available");
-        query = `SELECT Item.userID as userid, Item.ItemID, SubCategory.CategoryName as subcatname, SuperCategory.CategoryName as catname, SuperCategory.CategoryDescription, SubCategory.Brand, URL, usercart.userID AS incart
-      FROM Item
-      INNER JOIN Category SubCategory ON Item.CategoryID = SubCategory.CategoryID 
-      LEFT OUTER JOIN Category SuperCategory ON SubCategory.SuperCategoryID = SuperCategory.CategoryID 
-      LEFT OUTER JOIN Image ON SubCategory.CategoryID = Image.CategoryID
-      LEFT OUTER JOIN (SELECT * FROM Cart WHERE UserID = $2) AS usercart ON usercart.ItemID = Item.ItemID
-        WHERE (SubCategory.Brand LIKE $1)
-        OR (SubCategory.CategoryName LIKE $1)
-        OR (SuperCategory.CategoryName LIKE $1) AND (Item.userID = NULL);`
-        const search = req.query.search.toLowerCase();
-        req.session.user.search = search;
-        console.log(req.session.user.search);
-      // console.log(req.query)
-      // console.log(search);
-      console.log('Searching for ' + search + ' ... ');
-          db.any(query, [ 
-            '%' + search + '%',
-            req.session.user.userid
-          ])
-
-          .then(results => {
-              // console.log(results); // the results will be displayed on the terminal if the docker containers are running
-            // Send some parameters
-            res.render('pages/search', {query: search, results: results, filter: req.query.filter, userid: req.session.user.userid});
-            //print out/present the results etc
-          })
-          .catch(error => {
-          // Handle errors
-      res.render('pages/search', {query: search, results: [], message: 'Error'}); //{<JSON data required to render the page, if applicable>}
-      });
-      }
       else if(!req.query.search){ //if there is no search given...
         // const search = "";
-        res.render('pages/search',{query: search, filter: req.query.filter, userid: req.session.user.userid});
+        res.render('pages/search',{query: "", filter: req.query.filter, userid: req.session.user.userid});
       }
       else{
-        const search = req.query.search.toLowerCase();
-        req.session.user.search = search;
-        console.log(req.session.user.search);
-      // console.log(req.query)
-      // console.log(search);
-      console.log('Searching for ' + search + ' ... ');
-          db.any(query, [ 
-            '%' + search + '%',
-            req.session.user.userid
-          ])
-
-          .then(results => {
-              // console.log(results); // the results will be displayed on the terminal if the docker containers are running
-            // Send some parameters
-            res.render('pages/search', {query: search, results: results, filter: req.query.filter, userid: req.session.user.userid});
-            //print out/present the results etc
-          })
-          .catch(error => {
-          // Handle errors
-      res.render('pages/search', {query: search, results: [], message: 'Error'}); //{<JSON data required to render the page, if applicable>}
-      });
-    }
+        const search = req.query.search.toLowerCase();;
+        var query = `SELECT Item.userID as userid, Item.ItemID, SubCategory.CategoryName as subcatname, SuperCategory.CategoryName as catname, SuperCategory.CategoryDescription, SubCategory.Brand, URL, usercart.userID AS incart
+        FROM Item 
+        INNER JOIN Category SubCategory ON Item.CategoryID = SubCategory.CategoryID 
+        LEFT OUTER JOIN Category SuperCategory ON SubCategory.SuperCategoryID = SuperCategory.CategoryID 
+        LEFT OUTER JOIN Image ON SubCategory.CategoryID = Image.CategoryID
+        LEFT OUTER JOIN (SELECT * FROM Cart WHERE UserID = $2) AS usercart ON usercart.ItemID = Item.ItemID
+          WHERE (SubCategory.Brand LIKE $1
+          OR SubCategory.CategoryName LIKE $1
+          OR SuperCategory.CategoryName LIKE $1)
+        `
+        if(req.query.available == "available"){
+          console.log("entered available");
+          query += `AND Item.userID IS NULL
+          `;
+        }
+        query+= `GROUP BY Item.ItemID, Item.userID, Item.ItemID, SubCategory.CategoryName, SuperCategory.CategoryName, SuperCategory.CategoryDescription, SubCategory.Brand, URL, usercart.userID`
+        query += `;`;
+        db.any(query, [ 
+          '%' + search + '%',
+          req.session.user.userid
+        ])
+        .then(results => {
+            // console.log(results); // the results will be displayed on the terminal if the docker containers are running
+          // Send some parameters
+          res.render('pages/search', {query: search, results: results, available: req.query.available, userid: req.session.user.userid});
+          //print out/present the results etc
+        })
+        .catch(error => {
+        // Handle errors
+        res.render('pages/search', {query: search, results: [], message: 'Error'}); //{<JSON data required to render the page, if applicable>}
+        });
+      };
   });
 
 
@@ -415,7 +382,7 @@ const dbConfig = {
         }
       })
       .catch(function (err) {
-          res.render('pages/home',{message: 'Not Added'} );
+          res.render('pages/search',{message: 'Not Added'} );
       })
     });
     
@@ -438,7 +405,7 @@ const dbConfig = {
         }
       })
       .catch(function (err) {
-          res.render('pages/home',{message: 'Not Added'} );
+          res.render('pages/search',{message: 'Not Removed'} );
       })
     });
 
