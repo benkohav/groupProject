4a96c1be044c5e440af8506fbc156fca4a48ea7d
Charlotte Gorgemans <chgo2286@colorado.edu>
2022-11-28 13:26:51 -0700

    streamlining all base files

--- appCode/index.js
+++ appCode/index.js
@@ -21,6 +21,7 @@ const dbConfig = {
   };
 
   const user = {
+    userid: undefined,
     username: undefined,
     password: undefined,
     item: undefined,
@@ -62,7 +63,7 @@ const dbConfig = {
     //Rendering register
     app.get('/register', (req, res) => {
         res.render('pages/register'); //{<JSON data required to render the page, if applicable>}
-      });
+      }); 
 
     //Rendering profile page
     app.get("/profile", (req, res) => {
@@ -70,57 +71,88 @@ const dbConfig = {
       {
         res.render('pages/login',{message: 'Error. No user logged in currently.'} );
       }
-      var profileQuery = "SELECT * FROM userTable WHERE userTable.userName = $1";
-      console.log(req.session.user.username);
-      console.log('Testing');
+      var profileQuery = "SELECT * FROM userTable WHERE userTable.userID = $1";
+      var itemsCheckedOutQuery = `SELECT ItemID, EXTRACT(day FROM timeDue-NOW()) as day, ABS(EXTRACT(hour FROM timeDue-NOW())) as hour, ABS(EXTRACT(minute FROM timeDue-NOW())) as minute, CategoryName, CategoryDescription, URL
+          FROM (SELECT * FROM Item WHERE Item.UserID = $1) AS userItems
+          INNER JOIN Category ON userItems.CategoryID = Category.CategoryID 
+          LEFT OUTER JOIN Image ON Category.CategoryID = Image.CategoryID;`;
+      // console.log('Testing');
       db.any(profileQuery, [
-        req.session.user.username
+        req.session.user.userid
       ])
       .then(data => 
         {
-          // console.log(data.firstname);
+          // console.log(data);
           username = data[0].username;
           firstName = data[0].firstname;
           lastName = data[0].lastname;
           email = data[0].email;
           schoolYear =  data[0].schoolyear;
-        res.render("pages/profile", {
-          username,
-          firstName,
-          lastName,
-          email,
-          schoolYear,
-        });
+
+          db.any(itemsCheckedOutQuery, [
+            req.session.user.userid
+          ])
+          .then(Items => 
+            {
+              // console.log(Items);
+              res.render("pages/profile", {
+                username,
+                firstName,
+                lastName,
+                email,
+                schoolYear,
+                Items
+              });
+            })
+            .catch((err) => {
+              res.render("pages/profile", {
+                  Items: [],
+                  error: true,
+                  message: err.message,
+              });
+          });
+        // res.render("pages/profile", {
+        //   username,
+        //   firstName,
+        //   lastName,
+        //   email,
+        //   schoolYear,
+        // });
       })
       .catch(function (err) {
-        res.render('pages/login',{message: 'Error. No user logged in currently.'} );
+        res.render('pages/login',{message: 'Error. This user is not found.'} );
       })
     });
 
-    // updates the database after fields in profile page have been edited --> updating user variable is needed 
-    app.post('/profile', async (req, res) => {
-      const hash = await bcrypt.hash(req.body.password, 10);
 
-      var query1 = "UPDATE userTable SET username = $1, password = $2, firstName = $3, lastName = $4, email = $5, schoolYear = $6 where username =$7;";
+// updates the database after fields in profile page have been edited --> updating user variable is needed 
+    app.post('/profile/username', async (req, res) => {
+      // const hash = await bcrypt.hash(req.body.password, 10);
+
+      var query1 = "UPDATE userTable SET username = $1 WHERE userTable.username = $2;";
 
       db.any(query1, [ 
       req.body.username,
-      hash,
-      req.body.firstName,
-      req.body.lastName,
-      req.body.email,
-      req.body.schoolYear,
+      // hash,
+      // req.body.firstName,
+      // req.body.lastName,
+      // req.body.email,
+      // req.body.schoolYear,
+
       req.session.user.username,
     ])
       .then(function (data) {
-          console.log(req.body.schoolYear);
+          // console.log(req.body.schoolYear);
+          req.session.user.username = req.body.username;
+          // res.render('pages/profile',{message: 'Username successfully updated.'} );
           res.redirect('/profile');
+
       })
       .catch(function (err) {
-        res.render('pages/profile',{message: 'Error. Please try registering again.'} );
+        res.render('pages/profile',{message: 'Error. Please try updating username again.'} );
       })
-
     });
+
 
     //Rendering home
     app.get('/home', (req, res) => {
@@ -128,40 +160,110 @@ const dbConfig = {
       {
         res.render('pages/login',{message: 'Error. No user logged in currently.'} );
       }
-      res.render('pages/home'); //{<JSON data required to render the page, if applicable>}
+      const ITEMS = `SELECT COUNT(*) as num, Category.CategoryName, Category.Brand, Image.URL 
+      FROM History
+      RIGHT OUTER JOIN Item ON History.ItemID = Item.ItemID
+      JOIN Category ON Item.CategoryID = Category.CategoryID 
+      LEFT OUTER JOIN Image ON Category.CategoryID = Image.CategoryID 
+      GROUP By Category.CategoryID, Image.URL
+      ORDER BY COUNT(*) DESC LIMIT 5;`;
+      db.query(ITEMS)
+        .then((Items) => {
+          // console.log(Items)
+            res.render("pages/home", { Items });
+        })
+        .catch((err) => {
+            res.render("pages/home", {
+                Item: [],
+                error: true,
+                message: err.message,
+            });
+        });
     });
 
-    //Rendering help page
-    app.get('/items', (req, res) => {
+    //Rendering Cart
+    app.get('/cart', (req, res) => {
       if(!req.session.user)
       {
         res.render('pages/login',{message: 'Error. No user logged in currently.'} );
       }
-      res.render('pages/items'); //{<JSON data required to render the page, if applicable>}
-    });
+      else{
+        var query = `SELECT Item.ItemID, CategoryName, CategoryDescription, DurationName, URL, Item.userID
+          FROM (SELECT * FROM Cart WHERE userID = $1) AS usercart
+          INNER JOIN Item ON usercart.ItemID = Item.ItemID
+          INNER JOIN Category ON Item.CategoryID = Category.CategoryID 
+          LEFT OUTER JOIN Image ON Category.CategoryID = Image.CategoryID;`
 
-    //Rendering checkout
-    app.get('/checkout', (req, res) => {
-      if(!req.session.user)
-      {
-        res.render('pages/login',{message: 'Error. No user logged in currently.'} );
+          db.any(query, [ 
+            req.session.user.userid
+          ])
+          .then(cart => {
+            // console.log(cart)
+            res.render('pages/cart', {username: req.session.user.username, cart:cart});
+            //print out/present the results etc
+          })
+          .catch(error => {
+          // Handle errors
+            res.render('pages/cart', {username: req.session.user.username, message:error})
+      });
       }
-      res.render('pages/checkout'); //{<JSON data required to render the page, if applicable>}
     });
-
+  
     //Rendering search
     app.get('/search', (req, res) => {
-      res.render('pages/search', {results:undefined, query:undefined});
-    });
+      if(!req.session.user)
+      {
+        res.render('pages/login',{message: 'Error. No user logged in currently.'} );
+      }
+      else if(!req.query.search){ //if there is no search given...
+        // const search = "";
+        res.render('pages/search',{query: "", filter: req.query.filter, userid: req.session.user.userid});
+      }
+      else{
+        const search = req.query.search.toLowerCase();;
+        var query = `SELECT Item.userID as userid, Item.ItemID, SubCategory.CategoryName as subcatname, SuperCategory.CategoryName as catname, SuperCategory.CategoryDescription, SubCategory.Brand, URL, usercart.userID AS incart
+        FROM Item 
+        INNER JOIN Category SubCategory ON Item.CategoryID = SubCategory.CategoryID 
+        LEFT OUTER JOIN Category SuperCategory ON SubCategory.SuperCategoryID = SuperCategory.CategoryID 
+        LEFT OUTER JOIN Image ON SubCategory.CategoryID = Image.CategoryID
+        LEFT OUTER JOIN (SELECT * FROM Cart WHERE UserID = $2) AS usercart ON usercart.ItemID = Item.ItemID
+          WHERE (SubCategory.Brand LIKE $1
+          OR SubCategory.CategoryName LIKE $1
+          OR SuperCategory.CategoryName LIKE $1)
+        `
+        if(req.query.available == "available"){
+          console.log("entered available");
+          query += `AND Item.userID IS NULL
+          `;
+        }
+        query+= `GROUP BY Item.ItemID, Item.userID, Item.ItemID, SubCategory.CategoryName, SuperCategory.CategoryName, SuperCategory.CategoryDescription, SubCategory.Brand, URL, usercart.userID`
+        query += `;`;
+        db.any(query, [ 
+          '%' + search + '%',
+          req.session.user.userid
+        ])
+        .then(results => {
+            // console.log(results); // the results will be displayed on the terminal if the docker containers are running
+          // Send some parameters
+          res.render('pages/search', {query: search, results: results, available: req.query.available, userid: req.session.user.userid});
+          //print out/present the results etc
+        })
+        .catch(error => {
+        // Handle errors
+        res.render('pages/search', {query: search, results: [], message: 'Error'}); //{<JSON data required to render the page, if applicable>}
+        });
+      };
+  });
+
 
     //Register logic 
     app.post('/register', async (req, res) => {
         const hash = await bcrypt.hash(req.body.password, 10);
-
+        console.log(hash);
         var query = "INSERT INTO userTable (username, password, firstName, lastName, email, schoolYear) values ($1, $2, $3, $4, $5, $6);";
 
 
-        db.any(query, [ 
+    db.any(query, [
         req.body.username,
         hash,
         req.body.firstName,
@@ -186,23 +288,26 @@ const dbConfig = {
 
     
     //Login logic
-    app.post('/login', async (req, res) => {
-        //the logic goes here
-        // const match = await bcrypt.compare(req.body.password, user.password); //await is explained in #8
+  app.post('/login', async (req, res) => {
+    //the logic goes here
+    // const match = await bcrypt.compare(req.body.password, user.password); //await is explained in #8
 
-        var query = "SELECT password FROM userTable WHERE userName = $1 LIMIT 1;"
+    var query = "SELECT password, userID FROM userTable WHERE userName = $1 LIMIT 1;"
 
-        db.any(query, [ 
+    db.any(query, [
         req.body.username
-      ])
+    ])
         .then(async function (user) {
             // res.redirect('/login');
             const match = await bcrypt.compare(req.body.password, user[0].password);
             var u_name = req.body.username;
+
             if(match)
             {
                 req.session.user = {
+                    userid: user[0].userid,
                     username: u_name,
+                    search: ""
                   };
                   req.session.save();
                 res.redirect('/home');
@@ -214,49 +319,120 @@ const dbConfig = {
 
         })
         .catch(function (err) {
-          //{message: 'This username does not exist. Please register.'}
+            //{message: 'This username does not exist. Please register.'}
             // res.redirect('/register');
-            res.render('pages/register',{message: 'This username does not exist. Please register.'} );
+            res.render('pages/register', { message: 'This username does not exist. Please register.' });
         })
-
       });
-      app.post('/search', async (req, res) => {
-          console.log('Searching for ' + req.body.search.toLowerCase() + ' ... ');
-          var query = `SELECT userID, ItemID, SubCategory.CategoryName as subcatname, SuperCategory.CategoryName as catname, SuperCategory.CategoryDescription, URL
-          FROM Item 
-          INNER JOIN Category SubCategory ON Item.CategoryID = SubCategory.CategoryID 
-          LEFT OUTER JOIN Category SuperCategory ON SubCategory.SuperCategoryID = SuperCategory.CategoryID 
-          LEFT OUTER JOIN Image ON SubCategory.CategoryID = Image.CategoryID
-            WHERE ItemName LIKE $1
-            OR SubCategory.CategoryName LIKE $1
-            OR SuperCategory.CategoryName LIKE $1;`
-
-          db.any(query, [ 
-            '%' + req.body.search.toLowerCase() + '%'
-          ])
-
-          .then(results => {
-              // console.log(results); // the results will be displayed on the terminal if the docker containers are running
-            // Send some parameters
-            res.render('pages/search', {query: req.body.search.toLowerCase(), results: results});
-            //print out/present the results etc
-          })
-          .catch(error => {
-          // Handle errors
-      res.render('pages/search', {query: req.body.search.toLowerCase(), results: [], message: 'Error'}); //{<JSON data required to render the page, if applicable>}
-      });
-    });
 
     //Rendering home again when you checkout 
+    app.post('/checkout', async (req, res) => {
+      //check if user has overdue items
+      var itemcheck = "SELECT EXTRACT(day FROM timeDue-NOW()) as day FROM Item WHERE Item.UserID = $1";
+      db.any(itemcheck, [req.session.user.userid])
+      .then(async function (times) {
+        var overdue = 0;
+        times.forEach(function(Item){
+          if(Item.day < 0){
+            // console.log(Item.day)
+            overdue += 1;
+          }
+        });
+        if(overdue > 0){
+          res.render('pages/checkout',{message: 'You have ' + overdue + ' overdue items. Please return them or mark as missing and try again'} );
+        }
+        else{
+          var query = `Update Item 
+          SET UserID = $1, timeBorrowed = NOW(), timeDue = NOW()+usercart.Duration
+          FROM (SELECT * FROM Cart WHERE userID = $1) AS usercart
+          WHERE Item.ItemID = usercart.ItemID
+          AND Item.userID IS NULL;
+          DELETE FROM Cart WHERE userID = $1;`
+          db.any(query, [req.session.user.userid])
+          .then(async function (user) {
+            res.render('pages/checkout',{message: 'Checkout Successful'} );
+          })
+          .catch(function (err) {
+              res.render('pages/checkout',{message: 'Checkout failed'} );
+          })
+        }
+      })
+      .catch(function (err) {
+          res.render('pages/checkout',{message: 'Checkout failed'} );
+      })
+// check if items are available
+      
+    });
 
+
+    app.post("/cart/add", (req, res) => {
+      if(!req.session.user)
+      {
+        res.render('pages/login',{message: 'Error. No user logged in currently.'} );
+      }
+      var query = "INSERT INTO Cart (userID, ItemID, Duration, DurationName) VALUES ($1, $2, $3, $3)"
+      db.any(query, [
+      req.session.user.userid,
+      req.body.itemid,
+      req.body.duration
+    ])
+      .then(async function (user) {
+        if(req.body.returnto){
+          res.redirect(req.body.returnto)
+        }
+        else{
+        res.render('pages/search',{message: 'Added to Cart'} );
+        }
+      })
+      .catch(function (err) {
+          res.render('pages/search',{message: 'Not Added'} );
+      })
+    });
+    
+    app.post("/cart/delete", (req, res) => {
+      if(!req.session.user)
+      {
+        res.render('pages/login',{message: 'Error. No user logged in currently.'} );
+      }
+      var query = "DELETE FROM Cart WHERE userID = $1 and ItemID = $2"
+      db.any(query, [
+      req.session.user.userid,
+      req.body.itemid
+    ])
+      .then(async function (user) {
+        if(req.body.returnto){
+          res.redirect(req.body.returnto)
+        }
+        else{
+          res.render('pages/search',{message: 'Removed'} );
+        }
+      })
+      .catch(function (err) {
+          res.render('pages/search',{message: 'Not Removed'} );
+      })
+    });
+
+    app.post('/return', async (req, res) => {
+      //the logic goes here
+      // check if items are available
+      var query = `INSERT INTO History (userID, ItemID, timeBorrowed, timeReturned)
+      SELECT userID, ItemID, timeBorrowed, NOW() FROM Item WHERE ItemID = $1;
+      UPDATE Item SET userID = NULL, timeBorrowed = NULL, timeDue = NULL WHERE ItemID = $1;`
+      db.any(query, [req.body.itemid])
+      .then(async function (user) {
+        res.redirect('/profile');
+      })
+      .catch(function (err) {
+          res.render('pages/checkout',{message: 'Return failed'} );
+      })
+    });
     app.get("/logout", (req, res) => {
       req.session.destroy();
       res.render("pages/login", {message: 'Logged out Successfully'});
     });
 
-      app.listen(3000);
-      console.log('Server is listening on port 3000');
-
+  app.listen(3000);
+  console.log('Server is listening on port 3000');
 
 
 
@@ -296,4 +472,3 @@ const dbConfig = {
     //         // Handle errors
     //         res.render('pages/discover', {results: []});
     //         })
-    //   })
--- appCode/init_data/create.sql
+++ appCode/init_data/create.sql
@@ -1,56 +1,57 @@
--- DROP TABLE IF EXISTS userTable
+DROP TABLE IF EXISTS userTable;
 CREATE TABLE userTable (
     userID SERIAL PRIMARY KEY,
-    userName VARCHAR(45) NOT NULL,
+    userName VARCHAR(45) UNIQUE NOT NULL,
     password CHAR(60) NOT NULL,
     firstName VARCHAR(60),
     lastName VARCHAR(60),
     email VARCHAR(60),
     schoolYear VARCHAR(60)
 );
--- INSERT INTO userTable (userID, userName, password, email)
--- VALUES ('0', 'Tom B. Erichsen', 'password', 'email');
 
--- DROP TABLE IF EXISTS Category ;
+DROP TABLE IF EXISTS Category ;
 CREATE TABLE Category (
   CategoryID SERIAL PRIMARY KEY,
-  CategoryName VARCHAR(45),
-  CategoryDescription VARCHAR(200),
-  Brand VARCHAR(45)
+  CategoryName VARCHAR(100),
+  CategoryDescription VARCHAR(300),
+  SuperCategoryID INT REFERENCES Category (CategoryID),
+  Brand VARCHAR(100),
+  URL VARCHAR(2048)
 );
 -- INSERT INTO Category (CategoryID, CategoryName, CategoryDescription, Brand)
 -- VALUES ('70', 'Laptop', 'description', 'what brand');
 
--- -- DROP TABLE IF EXISTS Item ;
+DROP TABLE IF EXISTS Item ;
 CREATE TABLE IF NOT EXISTS Item (
   ItemId SERIAL PRIMARY KEY,
-  ItemName VARCHAR(45),
-  ItemDescription VARCHAR(200),
   Condition VARCHAR(200),
   CategoryID INT NOT NULL REFERENCES Category (CategoryID),
-  userID INT NOT NULL REFERENCES userTable (userID),
-  rentPerDay DECIMAL(10,2),
-  timeBorrowed INT,
-  timeReturned INT
+  userID INT REFERENCES userTable (userID),
+  timeBorrowed TIMESTAMP,
+  timeDue TIMESTAMP
 );
 
--- DROP TABLE IF EXISTS Image ;
-CREATE TABLE IF NOT EXISTS Image (
-  ImageID SERIAL PRIMARY KEY,
-  URL VARCHAR,
-  CategoryID INT NOT NULL REFERENCES Category (CategoryID)
-);
 -- INSERT INTO Image (ImageID, URL, CategoryID)
 -- VALUES ('1200', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.science.org%2Fcontent%2Farticle%2Fthese-adorable-puppies-may-help-explain-why-dogs-understand-our-body-language&psig=AOvVaw24OXFHBDcLqoDqDU5bIuIN&ust=1668149428389000&source=images&cd=vfe&ved=0CBAQjhxqFwoTCIjpsIODo_sCFQAAAAAdAAAAABAE', '70');
 
--- DROP TABLE IF EXISTS History ;
+DROP TABLE IF EXISTS History ;
 CREATE TABLE IF NOT EXISTS History (
+  HistoryKey SERIAL PRIMARY KEY UNIQUE NOT NULL,
   userID INT NOT NULL REFERENCES userTable (userID),
   ItemID INT NOT NULL REFERENCES Item (ItemId),
-  timeReturned TIME,
-  payBy DATE
+  timeBorrowed TIMESTAMP,
+  timeReturned TIMESTAMP,
+  payBy TIMESTAMP
 );
 
+DROP TABLE IF EXISTS Cart;
+CREATE TABLE IF NOT EXISTS Cart(
+  userID INT NOT NULL REFERENCES userTable (userID),
+  ItemID INT NOT NULL REFERENCES Item (ItemId),
+  Duration INTERVAL, 
+  DurationName VARCHAR(50),
+  CONSTRAINT CartID PRIMARY KEY (userID, ItemID)
+);
 -- INSERT INTO Image (userID, ItemID, timeReturned, payBy, histPK)
 -- VALUES ('1', '6000', '20:12:30', '2022-12-011', '16000');
 
@@ -96,10 +97,6 @@ CREATE TABLE IF NOT EXISTS History (
 
 -- INFO END
 
-
-
-
-
 -- CREATE TABLE users(
 --   userID PRIMARY KEY,
 --   username VARCHAR(45) NOT NULL,
@@ -109,41 +106,44 @@ CREATE TABLE IF NOT EXISTS History (
 -- );
 
 -- DROP TABLE IF EXISTS Category ;
--- CREATE TABLE IF NOT EXISTS Category (
+-- CREATE TABLE Category (
 --   CategoryID SERIAL PRIMARY KEY,
 --   CategoryName VARCHAR(45),
 --   CategoryDescription VARCHAR(200),
---   Brand VARCHAR(45),
---   SubcategoryID INT REFERENCES Category (CategoryID)
+--   Brand VARCHAR(45)
 -- );
+-- INSERT INTO Category (CategoryID, CategoryName, CategoryDescription, Brand)
+-- VALUES ('70', 'Laptop', 'description', 'what brand');
 
-
--- DROP TABLE IF EXISTS Item ;
+-- -- DROP TABLE IF EXISTS Item ;
 -- CREATE TABLE IF NOT EXISTS Item (
 --   ItemId SERIAL PRIMARY KEY,
 --   ItemName VARCHAR(45),
 --   ItemDescription VARCHAR(200),
 --   Condition VARCHAR(200),
 --   CategoryID INT NOT NULL REFERENCES Category (CategoryID),
---   userID INT NOT NULL REFERENCES User (userID),
---   timeBorrowed DATETIME,
---   timeReturned DATETIME
+--   userID INT NOT NULL REFERENCES userTable (userID),
+--   rentPerDay DECIMAL(10,2),
+--   timeBorrowed INT,
+--   timeReturned INT
 -- );
 
 -- DROP TABLE IF EXISTS Image ;
 -- CREATE TABLE IF NOT EXISTS Image (
 --   ImageID SERIAL PRIMARY KEY,
---   URL VARCHAR(45),
+--   URL VARCHAR,
 --   CategoryID INT NOT NULL REFERENCES Category (CategoryID)
 -- );
+-- INSERT INTO Image (ImageID, URL, CategoryID)
+-- VALUES ('1200', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.science.org%2Fcontent%2Farticle%2Fthese-adorable-puppies-may-help-explain-why-dogs-understand-our-body-language&psig=AOvVaw24OXFHBDcLqoDqDU5bIuIN&ust=1668149428389000&source=images&cd=vfe&ved=0CBAQjhxqFwoTCIjpsIODo_sCFQAAAAAdAAAAABAE', '70');
 
 -- DROP TABLE IF EXISTS History ;
 -- CREATE TABLE IF NOT EXISTS History (
---   userID INT NOT NULL REFERENCES User (userID),
+--   userID INT NOT NULL REFERENCES userTable (userID),
 --   ItemID INT NOT NULL REFERENCES Item (ItemId),
---   timeReturned DATETIME,
---   feeOutstanding TINYINT DEFAULT 0,
---   feeAmount DECIMAL(7,2),
---   payby DATETIME,
---   PRIMARY KEY (userID, ItemID)
+--   timeReturned TIME,
+--   payBy DATE
 -- );
+
+-- INSERT INTO Image (userID, ItemID, timeReturned, payBy, histPK)
+-- VALUES ('1', '6000', '20:12:30', '2022-12-011', '16000');
